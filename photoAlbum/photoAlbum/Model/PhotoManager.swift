c67b2d28d6d6640447eb17d044248090c4f73263
joho2022 <josama1997@naver.com>
2024-04-26 16:21:12 +0900

    refactor: 데이트 소스 별도 타입으로 분리

--- photoAlbum/photoAlbum/Model/PhotoManager.swift
+++ photoAlbum/photoAlbum/Model/PhotoManager.swift
@@ -5,4 +5,58 @@
 //  Created by 조호근 on 4/26/24.
 //
 
-import Foundation
+import UIKit
+import Photos
+
+class PhotoManager: NSObject {
+    enum NotificationNames {
+        static let didChangePhotos = Notification.Name("didChangePhotos")
+    }
+    private var allPhotos: PHFetchResult<PHAsset>!
+    private var imageManager: PHCachingImageManager!
+    
+    var count: Int {
+        return self.allPhotos.count
+    }
+    
+    override init() {
+        super.init()
+        self.fetchPhotos()
+        
+        switch PHPhotoLibrary.authorizationStatus() {
+        case .authorized:
+            PHPhotoLibrary.shared().register(self)
+        default:
+            return
+        }
+    }
+    
+    private func fetchPhotos() {
+        let allPhotosOptions = PHFetchOptions()
+        allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        allPhotos = PHAsset.fetchAssets(with: allPhotosOptions)
+        imageManager = PHCachingImageManager()
+    }
+    
+    func makePhotoData(index: Int, size: CGSize, completion: @escaping (UIImage?) -> Void) {
+        let asset = self.allPhotos.object(at: index)
+        imageManager.requestImage(for: asset,
+                                  targetSize: size,
+                                  contentMode: .aspectFill,
+                                  options: nil) { image, _ in
+            completion(image)
+        }
+    }
+}
+
+extension PhotoManager: PHPhotoLibraryChangeObserver {
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        DispatchQueue.main.async {
+            if let changes = changeInstance.changeDetails(for: self.allPhotos) {
+                self.allPhotos = changes.fetchResultAfterChanges
+    
+                NotificationCenter.default.post(name: NotificationNames.didChangePhotos, object: nil)
+            }
+        }
+    }
+}
